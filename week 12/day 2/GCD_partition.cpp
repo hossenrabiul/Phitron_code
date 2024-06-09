99bcd3ec509b772ac10fde331b0a463cfb8afd92
hossenrabiul <145250687+hossenrabiul@users.noreply.github.com>
2024-06-09 22:03:40 +0600

    Add files via upload
    week 12 code uploaded

---
+++ week 12/day 2/GCD_partition.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b){
+    return __gcd(a, b);
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        int sum = 0, sum1 = 0, ans = 1;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            sum += a[i];
+        }
+        for(int i = n - 1; i >= 1; i--){
+            sum -= a[i];
+            sum1 += a[i];
+            ans = max(ans, gcd(sum, sum1));
+        }
+    }
+    return 0;
+}
